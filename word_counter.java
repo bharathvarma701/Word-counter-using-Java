623d03f6b3d78f8537cfe89d643a3758c688873e
Bharath Varma Addepalli <120040683+bharathvarma701@users.noreply.github.com>
2024-03-25 15:13:48 +0530

    main file

--- word_counter.java
+++ word_counter.java
@@ -11,20 +11,17 @@ public class word_counter extends JFrame implements ActionListener {
     public word_counter() {
         super("Word Counter");
 
-        // Create text area and count button
         textArea = new JTextArea(10, 30);
         countButton = new JButton("Count");
-
-        // Create count label with initial value of zero
         countLabel = new JLabel("Word count: 0");
-        // Add components to frame
+       
         JPanel panel = new JPanel();
         panel.add(new JScrollPane(textArea));
         panel.add(countButton);
         panel.add(countLabel);
         add(panel);
 
-        // Register action listener for count button
+        
         countButton.addActionListener(this);
 
         // Set frame properties
@@ -36,10 +33,7 @@ public class word_counter extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         String text = textArea.getText();
-
         String[] words = text.split("\\s+");
-
-        
         int count = words.length;
 
         // Update count label
